57af593f7d20f7fae4e06b8b64f85e537e3dc851
Nazar Syafiq <nazarsyafiq89@gmail.com>
2025-12-24 19:51:55 +0800

    Remove top navigation bar for cleaner UI

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -21,27 +21,7 @@
 
 <body class="bg-light">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-white bg-white shadow-sm sticky-top">
-        <div class="container">
-            <a class="navbar-brand fw-bold" href="#">NS</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"
-                            @click.prevent="$dispatch('open-modal', { component: 'about' })">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"
-                            @click.prevent="$dispatch('open-modal', { component: 'contact' })">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+
 
     <!-- Main Content -->
     <main class="container py-4">
